d947d11f0ddddc37d0e67f12afa78f7391dd5f98
pwohl <pwohland@gmail.com>
2014-07-13 21:38:30 +0100

    Plot 2 Script

---
+++ Plot2.R
@@ -0,0 +1,29 @@
+##Plot 2
+Hh.Po.Co<-read.table("household_power_consumption.txt",
+                     header = TRUE,
+                     nrows=2075259,
+                     sep=";",
+                     na.strings = "?")
+
+
+#Convert Date and Time variables
+attach(Hh.Po.Co)
+x <- paste(Date, Time)
+Date.Time<-strptime(x, "%d/%m/%Y %H:%M:%S")
+Hh.Po.Co.data<-cbind(Date.Time,Hh.Po.Co)
+detach(Hh.Po.Co)
+
+#Subset the data
+Hh.Po.Co.subdata<-rbind(Hh.Po.Co.data[Hh.Po.Co.data$Date=="1/2/2007",],
+                        Hh.Po.Co.data[Hh.Po.Co.data$Date=="2/2/2007",])
+
+
+
+#Plot 2
+with(Hh.Po.Co.subdata,
+     plot(Date.Time,Global_active_power,
+          type="l",
+          ylab="Global Acitve Power (kilowatts)",
+          xlab=""))
+
+dev.copy(png, file = "plot2.png")
